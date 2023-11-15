40e0935a2bbd16dc474cc0b0e3676299427e56e8
Anderson Assum <anderson.assum12@gmail.com>
2023-11-15 20:29:20 -0300

    att

---
+++ Aula_2/script.js
@@ -0,0 +1,12 @@
+//tech guide 
+
+
+var numeroSecreto = 581
+
+var chute = prompt("Digite um número entre 1 e 1000")
+
+if (chute == numeroSecreto) {
+    alert('Você acertou! Parabéns.')
+} else {
+    alert('Você errou. Tente novamente')
+} else 
