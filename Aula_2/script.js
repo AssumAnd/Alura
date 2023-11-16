778e5dd9c09559ff00d43eabbd9fb7a30a817849
Anderson Assum <anderson.assum12@gmail.com>
2023-11-16 13:45:02 -0300

    Atualização de código

--- Aula_2/script.js
+++ Aula_2/script.js
@@ -3,22 +3,37 @@
 //While = enquanto
 
 //tech guide 
-//if também é considerado comando de fluxo
+//if também é considerado controle de fluxo
 
-var numeroSecreto = 581
+//oInt de parseInt significa INTEIRO. Em caso de 865,99 o parseInt busca somente o 865
 
 
-while (chute != numeroSecreto) {
-    var chute = prompt("Digite um número entre 1 e 1000")
-    if (chute == numeroSecreto) {
-        alert('Você acertou! Parabéns.')
-    } else if (chute > numeroSecreto) {
-        alert('Errou. O número secreto é menor')
-    } else if (chute < numeroSecreto) {
-        alert('Errou. O número secreto é maior')
+function advinha() {
+
+    var numeroSecreto = parseInt(Math.random() * 1001)
+    var numeroSecreto = 1002
+    while (chute != numeroSecreto) {
+        contador == ++contador
+        if (contador > 10) {
+            alert('Limite excedido. Tente novamente')
+            break
+        }
+        var chute = prompt("Digite um número entre 1 e 1000")
+
+        if (chute == numeroSecreto) {
+            alert('Você acertou! Parabéns. Você precisou de ' + contador + " chances.")
+            if (contador < 3) {
+                alert("GÊNIO DA BOLA, O PELÉ DOS CHUTES.")
+            }
+        } else if (chute > numeroSecreto) {
+            alert('Errou. O número secreto é menor que ' + chute)
+        } else if (chute < numeroSecreto) {
+            alert('Errou. O número secreto é maior que ' + chute)
+        }
     }
-
 }
+
+
 
 
 /*else {
