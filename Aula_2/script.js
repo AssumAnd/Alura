440129612d664435637c1a8dc85acbb16095fe7c
Anderson Assum <anderson.assum12@gmail.com>
2023-11-16 10:27:48 -0300

    Atualização do script

--- Aula_2/script.js
+++ Aula_2/script.js
@@ -1,12 +1,26 @@
-//tech guide 
+//AULA 2 - Condicionais, Laços de repetição e Funções
 
+//While = enquanto
+
+//tech guide 
+//if também é considerado comando de fluxo
 
 var numeroSecreto = 581
 
-var chute = prompt("Digite um número entre 1 e 1000")
 
-if (chute == numeroSecreto) {
-    alert('Você acertou! Parabéns.')
-} else {
+while (chute != numeroSecreto) {
+    var chute = prompt("Digite um número entre 1 e 1000")
+    if (chute == numeroSecreto) {
+        alert('Você acertou! Parabéns.')
+    } else if (chute > numeroSecreto) {
+        alert('Errou. O número secreto é menor')
+    } else if (chute < numeroSecreto) {
+        alert('Errou. O número secreto é maior')
+    }
+
+}
+
+
+/*else {
     alert('Você errou. Tente novamente')
-} else 
+} */
